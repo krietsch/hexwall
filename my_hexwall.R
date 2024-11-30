0232df6b1ce0ff0a5140d9ab92a5f06bdaf85e9a
Johannes Krietsch <johannes.krietsch@nioz.nl>
2024-11-30 21:51:00 +0100

    created bmy own hexwall

---
+++ my_hexwall.R
@@ -0,0 +1,8 @@
+#===============================================================================
+# Create my own hexwall
+#===============================================================================
+
+source("hexwall.R")
+
+hexwall("myhex", sticker_row_size = 7, sticker_width = 200, 
+        sort_mode = "colour", background_color = NA)
